fb7dc410c3369eb9a0eed9ec38b0a184ea79e150
Fortune <chihurumikechukwu05@gmail.com>
2024-11-11 09:54:54 +0100

    fixing images

--- src/app/projects/projectcard.component.ts
+++ src/app/projects/projectcard.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component ,Input} from "@angular/core";
 
 @Component({
     selector:'project-card',
@@ -8,5 +8,5 @@ import { Component } from "@angular/core";
     // styleUrl:""
 })
 export class ProjectCard{
-
+    @Input() src:string=""
 }
